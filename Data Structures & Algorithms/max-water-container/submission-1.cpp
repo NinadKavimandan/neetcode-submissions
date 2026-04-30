29827102de0b7e03f8d1780e5de1f7706fb3986e
Ninad Kavimandan <ninadkavimandan@github.com>
2026-04-30 00:09:54 -0700

    Add: max-water-container - submission-1

---
+++ Data Structures & Algorithms/max-water-container/submission-1.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int maxArea(vector<int>& heights) 
+    {
+        int maximum = 0;
+        int start = 0;
+        int end = heights.size() - 1;
+        
+        while (start < end)
+        {
+            maximum = max(maximum, min(heights[start], heights[end])*(end-start));
+            if (heights[start] < heights[end])
+            {
+                start++;
+            }
+            else
+            {
+                end--;
+            }
+        }
+
+        return maximum;
+    }
+};
+
+/*
+
+let's give this a try with two pointers before i go to bed.
+start is 0, end is last index.
+you move the lesser height each time, compute area, see if that's greater than current max.
+
+*/
