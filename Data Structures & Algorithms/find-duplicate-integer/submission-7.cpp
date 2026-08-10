740d6b1fe1a63d56589f0b0d9b8a15bcdce27a21
Ninad Kavimandan <ninadkavimandan@github.com>
2026-08-09 23:07:17 -0700

    Add: find-duplicate-integer - submission-7

---
+++ Data Structures & Algorithms/find-duplicate-integer/submission-7.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums)
+    {
+        vector<bool> seen(10001, false);
+        for (int num : nums)
+        {
+            if (seen[num])
+                return num;
+            seen[num] = true;
+        }
+
+        return -1;
+    }
+};
+
+/*
+
+we know both the lower and upper bounds of n, we can use a fixed size array of booleans to store the seen map, and identify the element already traversed
+
+*/
