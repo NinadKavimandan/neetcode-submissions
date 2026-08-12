638de18bdbf5c09097b5970876c5b8f8b4c2a391
Ninad Kavimandan <ninadkavimandan@github.com>
2026-08-12 15:22:37 -0700

    Add: reverse-nodes-in-k-group - submission-0

---
+++ Data Structures & Algorithms/reverse-nodes-in-k-group/submission-0.cpp
@@ -0,0 +1,60 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+class Solution {
+public:
+    ListNode* reverseKGroup(ListNode* head, int k)
+    {
+        ListNode* headGroupReversed = new ListNode();
+        ListNode* iter = headGroupReversed;
+        ListNode* slow = head;
+        ListNode* fast = head;
+        ListNode* prev = nullptr;
+
+        while (fast != nullptr)
+        {
+            for (int i=0;i < k;i++)
+            {
+                if (fast == nullptr)
+                {
+                    iter->next = slow;
+                    return headGroupReversed->next;
+                }
+
+                fast = fast->next;
+            }
+
+            ListNode* iterSlow = slow;
+            while (iterSlow != fast)
+            {
+                ListNode* temp = iterSlow->next;
+                iterSlow->next = prev;
+                prev = iterSlow;
+                iterSlow = temp;
+            }
+
+            iter->next = prev;
+            iter = slow;
+            prev = slow;
+            slow = fast;
+        }
+
+        iter->next = nullptr;
+
+        return headGroupReversed->next;
+    }
+};
+
+/*
+
+we can use two pointers k nodes apart, then bring the left pointer towards right while reversing the list. Use a dummy as head, use a pointer iter pointing to the dummy to curate the list. Each time the list is reversed, assign iter->next = fast, if not reversed, iter->next = slow, at the end, if list is reversed, slow->next = nullptr. otherwise fast->next = nullptr (this would be implicit).
+
+*/
