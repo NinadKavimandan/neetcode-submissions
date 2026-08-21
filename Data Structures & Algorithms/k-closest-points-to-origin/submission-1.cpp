0cd874d838d706d2fa13d705c943dabae0174d62
Ninad Kavimandan <ninadkavimandan@github.com>
2026-08-20 23:04:41 -0700

    Add: k-closest-points-to-origin - submission-1

---
+++ Data Structures & Algorithms/k-closest-points-to-origin/submission-1.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    struct CustomComparator
+    {
+        bool operator()(const pair<double, int>& a, const pair<double, int>& b)
+        {
+            return a.first < b.first;
+        };
+    };
+
+    vector<vector<int>> kClosest(vector<vector<int>>& points, int k)
+    {
+        priority_queue<pair<double, int>, vector<pair<double, int>>, CustomComparator> maxHeap;
+
+        for (int i=0;i < points.size();i++)
+        {
+            double distance = sqrt(points[i][0]*points[i][0] + points[i][1]*points[i][1]);
+            maxHeap.push({distance, i});
+
+            if (maxHeap.size() > k)
+            {
+                maxHeap.pop();
+            }
+        }
+
+        vector<vector<int>> response;
+
+        while (!maxHeap.empty())
+        {
+            response.push_back(points[maxHeap.top().second]);
+            maxHeap.pop();
+        }
+
+        return response;
+    }
+};
+
+/*
+
+use a max heap to maintain k points closest to the origin
+maintain <distance, index in points vector> with each entry in the heap to avoid recomputation and fast lookup.
+
+*/
