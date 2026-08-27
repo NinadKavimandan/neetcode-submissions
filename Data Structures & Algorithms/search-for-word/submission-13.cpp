e7de631ee9441d697ef5e47fac0fb02cbfa52a49
Ninad Kavimandan <ninadkavimandan@github.com>
2026-08-27 13:25:40 -0700

    Add: search-for-word - submission-13

---
+++ Data Structures & Algorithms/search-for-word/submission-13.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    bool iterate (vector<vector<char>>& board, const string& word, int curCharPos, int cellRow, int cellCol)
+    {
+
+        if (cellRow < 0 || cellRow >= board.size() || cellCol < 0 || cellCol >= board[0].size())
+        {
+            return false;
+        }
+
+        if (board[cellRow][cellCol] != word[curCharPos])
+        {
+            return false;
+        }
+
+        if (curCharPos == word.size() - 1)
+        {
+            return true;
+        }
+
+        char temp = board[cellRow][cellCol];
+        board[cellRow][cellCol] = '.';
+        bool result = iterate(board, word, curCharPos + 1, cellRow + 1, cellCol) || iterate(board, word, curCharPos + 1, cellRow, cellCol + 1) || iterate(board, word, curCharPos + 1, cellRow - 1, cellCol) || iterate(board, word, curCharPos + 1, cellRow, cellCol - 1);
+        board[cellRow][cellCol] = temp;
+        
+        return result;
+    }
+
+    bool exist (vector<vector<char>>& board, string word)
+    {
+        for (int row = 0;row < board.size();row++)
+        {
+            for (int col = 0;col < board[0].size();col++)
+            {
+                if (iterate(board, word, 0, row, col))
+                {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+};
+
+/*
+
+iterate over the word and use dfs to iterate each path on the board, enter a branch with next character is current matches, otherwise move to next to start search from first character again.
+
+*/
