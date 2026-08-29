e89ab08651a45d5d3306406e71273cdb52893b96
Ninad Kavimandan <ninadkavimandan@github.com>
2026-08-29 12:28:22 -0700

    Add: n-queens - submission-5

---
+++ Data Structures & Algorithms/n-queens/submission-5.cpp
@@ -0,0 +1,67 @@
+class Solution {
+public:
+    vector<bool> unsafeCol = {false, false, false, false, false, false, false, false};
+    vector<bool> diag = {false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false};
+    vector<bool> altDiag = {false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false};
+    
+    void addQueen (vector<string>& board, int row, int col)
+    {
+        unsafeCol[col] = true;
+        diag[row + col] = true;
+        altDiag[row - col + 7] = true;
+        board[row][col] = 'Q';
+    }
+
+    void removeQueen (vector<string>& board, int row, int col)
+    {
+        unsafeCol[col] = false;
+        diag[row + col] = false;
+        altDiag[row - col + 7] = false;
+        board[row][col] = '.';
+    }
+
+    bool isSafe (vector<string>& board, int row, int col)
+    {
+        if (board[row][col] != '.' || unsafeCol[col] || diag[row + col] || altDiag[row - col + 7])
+        {
+            return false;
+        }
+
+        return true;
+    }
+
+    void generate (vector<string>& board, int row, vector<vector<string>>& response)
+    {
+        if (row == board.size())
+        {
+            response.push_back(board);
+            return;
+        }
+
+        for (int i=0;i < board.size();i++)
+        {
+            if (isSafe(board, row, i))
+            {
+                addQueen(board, row, i);
+                generate(board, row + 1, response);
+                removeQueen(board, row, i);
+            }
+        }
+    }
+
+    vector<vector<string>> solveNQueens(int n)
+    {
+        vector<string> board(n, string(n, '.'));
+        vector<vector<string>> response;
+        generate(board, 0, response);
+        return response;
+    }
+};
+
+/*
+
+start simple, n queens over nxn board means, no more than 1 queen per row and per column, the queens cannot be diagonally aligned either.
+
+this decreases the search space, go row by row, decide the position of the queen in that row, assume that position and see if you can place the queen in the next row successfully. backtrack if you cannot.
+
+*/
