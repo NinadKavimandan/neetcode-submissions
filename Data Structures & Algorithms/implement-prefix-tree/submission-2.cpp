23e54ea8ec78b9ce6f3fed7c0c3c0f9f41d5ad34
Ninad Kavimandan <ninadkavimandan@github.com>
2026-08-29 22:28:17 -0700

    Add: implement-prefix-tree - submission-2

---
+++ Data Structures & Algorithms/implement-prefix-tree/submission-2.cpp
@@ -0,0 +1,72 @@
+class PrefixTree {
+public:
+    typedef struct Trie
+    {
+        vector<Trie*> next;
+        bool wordEnd;
+
+        Trie() : next(vector<Trie*>(26, nullptr)), wordEnd(false) {}
+    } Trie;
+
+    Trie* root;
+
+    PrefixTree()
+    {
+        root = new Trie();
+    }
+    
+    void insert(string word)
+    {
+        Trie* iter = root;
+        for (char c : word)
+        {
+            if (iter->next[c - 'a'] == nullptr)
+            {
+                iter->next[c - 'a'] = new Trie();
+            }
+            iter = iter->next[c - 'a'];
+        }
+
+        iter->wordEnd = true;
+    }
+    
+    bool search(string word)
+    {
+        Trie* iter = root;
+
+        for (char c : word)
+        {
+            if (iter->next[c - 'a'] == nullptr)
+            {
+                return false;
+            }
+
+            iter = iter->next[c - 'a'];
+        }
+
+        return iter != nullptr ? iter->wordEnd : false;
+    }
+    
+    bool startsWith(string prefix)
+    {
+        Trie* iter = root;
+
+        for (char c : prefix)
+        {
+            if (iter->next[c - 'a'] == nullptr)
+            {
+                return false;
+            }
+
+            iter = iter->next[c - 'a'];
+        }
+
+        return true;
+    }
+};
+
+/*
+
+create a trie with a structure containing the character, 26 pointers for each character, and a flag to suggest if its the end of a word.
+
+*/
