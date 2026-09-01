a76f40f9515328249901ab15333b26047c6244ec
Ninad Kavimandan <ninadkavimandan@github.com>
2026-09-01 11:06:09 -0700

    Add: clone-graph - submission-3

---
+++ Data Structures & Algorithms/clone-graph/submission-3.cpp
@@ -0,0 +1,61 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> neighbors;
+    Node() {
+        val = 0;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val) {
+        val = _val;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val, vector<Node*> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};
+*/
+
+class Solution {
+public:
+    void dfs (Node* node, unordered_map<int, Node*>& cloneNodeMap)
+    {
+        cloneNodeMap[node->val] = new Node(node->val);
+
+        for (Node* neighbor : node->neighbors)
+        {
+            if (cloneNodeMap.find(neighbor->val) == cloneNodeMap.end())
+            {
+                dfs(neighbor, cloneNodeMap);
+            }
+
+            cloneNodeMap[node->val]->neighbors.push_back(cloneNodeMap[neighbor->val]);
+        }
+    }
+
+    Node* cloneGraph(Node* node)
+    {
+        if (node == nullptr)
+        {
+            return nullptr;
+        }
+
+        unordered_map<int, Node*> cloneNodeMap;
+        dfs(node, cloneNodeMap);
+
+        return cloneNodeMap[node->val];
+    }
+};
+
+/*
+
+its a connected graph, so traversing from any node should take us through all nodes eventually.
+
+each node value is numbered to 1 to n, for n nodes, this makes it easier to store the nodes already cloned by the node value.
+
+to start with, go through each node and its neighbours list, for each neighbor, traverse recursively till you reach the end of the graph, maintain a visited set to avoid duplicate traversals, also create clone of each node you visit. on return from recursion, populate adjacency lists with the clones you created. maintain the clones in a map for quick retrieval.
+
+*/
