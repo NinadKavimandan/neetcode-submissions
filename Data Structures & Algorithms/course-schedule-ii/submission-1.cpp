a03a1092191f3c6cb53acf38e4645d17cd8f7655
Ninad Kavimandan <ninadkavimandan@github.com>
2026-09-04 23:46:45 -0700

    Add: course-schedule-ii - submission-1

---
+++ Data Structures & Algorithms/course-schedule-ii/submission-1.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites)
+    {
+        vector<int> order;
+        vector<vector<int>> adjList(numCourses, vector<int>());
+        vector<int> preReqsLeft(numCourses, 0);
+        queue<int> courseQueue;
+
+        for (vector<int>& item : prerequisites)
+        {
+            adjList[item[1]].push_back(item[0]);
+            preReqsLeft[item[0]]++;
+        }
+
+        for (int i=0;i < numCourses;i++)
+        {
+            if (preReqsLeft[i] == 0)
+            {
+                courseQueue.push(i);
+            }
+        }
+
+        while (!courseQueue.empty())
+        {
+            int course = courseQueue.front();
+            order.push_back(course);
+            for (int nextCourse : adjList[course])
+            {
+                preReqsLeft[nextCourse]--;
+                if (preReqsLeft[nextCourse] == 0)
+                {
+                    courseQueue.push(nextCourse);
+                }
+            }
+
+            courseQueue.pop();
+        }
+
+        return order.size() == numCourses ? order : vector<int>();
+    }
+};
+
+/*
+
+use Kahn's topological sort to always remove the course with no pending pre-requisite (0 in-degree). return the order of courses enqueued only if there is no course 0 in-degree left.
+
+*/
