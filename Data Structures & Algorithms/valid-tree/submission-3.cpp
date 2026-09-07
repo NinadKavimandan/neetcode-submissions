0670f28089d974f25426c39936d56e9ffde86062
Ninad Kavimandan <ninadkavimandan@github.com>
2026-09-06 23:51:14 -0700

    Add: valid-tree - submission-3

---
+++ Data Structures & Algorithms/valid-tree/submission-3.cpp
@@ -0,0 +1,55 @@
+class Solution {
+public:
+    bool dfs (int current, vector<bool>& visited, vector<vector<int>>& adjList, int& nodesProcessed, int parent)
+    {
+        bool response = true;
+        visited[current] = true;
+        nodesProcessed++;
+
+        for (int next : adjList[current])
+        {
+            if (next != parent)
+            {
+                if (visited[next])
+                {
+                    return false;
+                }
+
+                if (!dfs(next, visited, adjList, nodesProcessed, current))
+                {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    bool validTree(int n, vector<vector<int>>& edges)
+    {
+        vector<vector<int>> adjList(n, vector<int>());
+        vector<bool> visited(n, false);
+        int nodesProcessed = 0;
+
+        for (vector<int>& edge : edges)
+        {
+            adjList[edge[0]].push_back(edge[1]);
+            adjList[edge[1]].push_back(edge[0]);
+        }
+
+        if (!dfs(0, visited, adjList, nodesProcessed, -1))
+        {
+            return false;
+        }
+
+        return nodesProcessed == n;
+    }
+};
+
+/*
+
+directed acyclic graph = tree, basically detect if there's cycle and if all nodes are connected.
+
+how do we detect a cycle though? may be do a dfs on each node you are connected to, if you ever encounter a node you already have seen before, it means you have cycled.
+
+*/
