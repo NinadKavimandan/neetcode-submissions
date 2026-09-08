d9b80acbf2ac51a9019fe9214fbd2a454bc5437e
Ninad Kavimandan <ninadkavimandan@github.com>
2026-09-08 11:47:06 -0700

    Add: redundant-connection - submission-3

---
+++ Data Structures & Algorithms/redundant-connection/submission-3.cpp
@@ -0,0 +1,51 @@
+class Solution {
+public:
+    bool isCyclic (vector<vector<int>>& adjList, int node, int parent, vector<bool>& visited)
+    {
+        bool response = false;
+        visited[node] = true;
+
+        for (int next : adjList[node])
+        {
+            if (visited[next])
+            {
+                if (next == parent)
+                {
+                    continue;
+                }
+                
+                return true;
+            }
+            else
+            {
+                response &= isCyclic(adjList, next, node, visited);
+            }
+        }
+
+        return response;
+    }
+
+    vector<int> findRedundantConnection(vector<vector<int>>& edges)
+    {
+        vector<vector<int>> adjList(edges.size(), vector<int>());
+        for (vector<int>& edge : edges)
+        {
+            vector<bool> visited(edges.size(), false);
+            adjList[edge[0] - 1].push_back(edge[1] - 1);
+            adjList[edge[1] - 1].push_back(edge[0] - 1);
+
+            if (isCyclic(adjList, edge[0] - 1, -1, visited))
+            {
+                return edge;
+            }
+        }
+
+        return vector<int>();
+    }
+};
+
+/*
+
+use dfs to traverse each path, if you encounter a node already visited, that's a redundant edge. since its a fully connected setup, you can choose any point to discover the redundant edge/s. For appearing last in the edges list, we can probably use extra memory as a boolean map to track what all edges are redundant, return first from right thats redundant.. too complex, just traverse from last node?
+
+*/
